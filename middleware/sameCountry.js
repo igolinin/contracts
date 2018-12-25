a4dd0f42c089e279a7b1dcee01514a3c0955cf3a
igolinin@gmail.com <igolinin@gmail.com>
2018-12-25 06:56:57 +0100

    new middleware

--- middleware/sameCountry.js
+++ middleware/sameCountry.js
@@ -9,7 +9,7 @@ module.exports = async function(req, res, next) {
   try {
     const decoded = jwt.verify(token, process.env.JWT);
     client = await profile(req.client);
-    console.log(req.client);
+    console.log(client);
     if (decoded.country != client.country)
       return res.status(403).send("countries don't match");
     next();
