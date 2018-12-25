7a0d6bee4d8340ada9cebae2ef76091f3476d16b
igolinin@gmail.com <igolinin@gmail.com>
2018-12-25 05:18:29 +0100

    structure and one route

--- middleware/manager.js
+++ middleware/manager.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const manager = require("../utils/getProfile");
 
 module.exports = function(req, res, next) {
   const token = req.header("x-auth-token");
@@ -8,6 +9,7 @@ module.exports = function(req, res, next) {
   try {
     const decoded = jwt.verify(token, process.env.JWT);
     if (decoded.role != "manager") return res.status(403).send("access denied");
+    req.manager = manager(decoded.email);
     next();
   } catch (ex) {
     res.status(400).send("invalid token");
