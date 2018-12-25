d11376103f02f3ebe10ba920c7386300e0119271
igolinin@gmail.com <igolinin@gmail.com>
2018-12-25 13:50:42 +0100

    new middleware

--- middleware/manager.js
+++ middleware/manager.js
@@ -2,14 +2,14 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const manager = require("../utils/getProfile");
 
-module.exports = function(req, res, next) {
+module.exports = async function(req, res, next) {
   const token = req.header("x-auth-token");
   if (!token) return res.status(401).send("Access denied, No token present");
 
   try {
     const decoded = jwt.verify(token, process.env.JWT);
     if (decoded.role != "manager") return res.status(403).send("access denied");
-    req.manager = manager(decoded.email);
+    req.manager = await manager(decoded.email);
     next();
   } catch (ex) {
     res.status(400).send("invalid token");
