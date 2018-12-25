77e9057677dff2faa484190c45b38a25b48c8a05
igolinin@gmail.com <igolinin@gmail.com>
2018-12-25 08:01:53 +0100

    new middleware

--- utils/getProfile.js
+++ utils/getProfile.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const profileUrl = process.env.PROFILE_SERVICE_HOST;
 
-module.exports = async function(email) {
+module.exports = async function getProfile(email) {
   const profile = await axios.get(
     `http://${profileUrl}:9090/api/v1/service/user/${email}`
   );
