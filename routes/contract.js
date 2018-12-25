9d929e7f8adaf80ebc1117cdad2f7ae2b4a0e82b
igolinin@gmail.com <igolinin@gmail.com>
2018-12-25 15:47:03 +0100

    new update routes

--- routes/contract.js
+++ routes/contract.js
@@ -23,7 +23,7 @@ router.post("/new", [manager, sameCountry, time], async (req, res) => {
 router.put("/total", manager, async (req, res) => {
   let contract = await Contract.findOne({ _id: req.body._id });
   if (!contract) return res.status(400).send("contract not found");
-  if (contract.manager != req.manager.email)
+  if (contract.manager != req.manager)
     return res.status(403).send("not authorized to update this contract");
   await Contract.findByIdAndUpdate(req.body._id, {
     $set: { total: req.body.total }
